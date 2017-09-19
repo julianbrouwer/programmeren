af8f8c1e762dcb842a8a58a87997ab27a999aa7a
Julian <julian.brouwer@student.hu.nl>
2017-09-19 14:51:08 +0200

    les 1,2,3,4

--- les 4/final assignment.py
+++ les 4/final assignment.py
@@ -1,1 +1,25 @@
-def standaardprijs(afstandKM)
+def standaardprijs(afstandKM):
+    if afstandKM > 50:
+        return afstandKM*0.60 + 15
+
+    else:
+        if afstandKM <=0:
+            return 0
+        return afstandKM*0.80
+
+
+def ritprijs(leeftijd, weekendrit, afstandKM):
+    standaardprijs(afstandKM)
+    if leeftijd < 12 or leeftijd >= 65:
+        if weekendrit == False:
+            return standaardprijs(afstandKM) * 0.70
+        else:
+            return standaardprijs(afstandKM) * 0.65
+    else:
+        if weekendrit == False:
+            return standaardprijs(afstandKM)
+        else:
+            return standaardprijs(afstandKM)*0.60
+print(ritprijs(11,False,10))
+
+
