373f90d33bac360302c41cd7c07b60c1556d9d20
Julian <julian.brouwer@student.hu.nl>
2017-10-17 16:26:04 +0200

    les 10

--- les 10/pe10_1.py
+++ les 10/pe10_1.py
@@ -1,1 +1,9 @@
 import xmltodict
+with open('artikelen.xml','r') as file:
+    inhoud=file.read()
+    artikelen=xmltodict.parse(inhoud)
+    for artikelen in file:
+        print(artikelen['naam'])
+
+print(artikelen)
+
